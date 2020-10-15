bd0a4ef8ef467d029e1844758226bfb9611e8301
Ettore Di Giacinto <mudler@users.noreply.github.com>
2020-10-15 18:24:43 +0200

    Don't use PAT

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 registration_url="https://github.com/${GITHUB_OWNER}"
-if [ -z "${GITHUB_REPOSITORY}" ]; then
-    token_url="https://api.github.com/orgs/${GITHUB_OWNER}/actions/runners/registration-token"
-else
-    token_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
+
+if [ -n "${GITHUB_REPOSITORY}" ]; then
     registration_url="${registration_url}/${GITHUB_REPOSITORY}"
 fi
-
-echo "Requesting token at '${token_url}'"
-
-payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${token_url})
-export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 
 if [ -z "${RUNNER_NAME}" ]; then
     RUNNER_NAME=$(hostname)
@@ -25,15 +18,8 @@ fi
     --unattended \
     --replace
 
-remove() {
-    payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${token_url%/registration-token}/remove-token)
-    export REMOVE_TOKEN=$(echo $payload | jq .token --raw-output)
-
-    ./config.sh remove --unattended --token "${REMOVE_TOKEN}"
-}
-
-trap 'remove; exit 130' INT
-trap 'remove; exit 143' TERM
+trap 'exit 130' INT
+trap 'exit 143' TERM
 
 ./run.sh "$*" &
 
